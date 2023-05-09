7c54f10e0beed2d6afe2f5a6bae3c310e5f067e5
aprakash97 <prakashakrakr@gmail.com>
2023-05-09 10:52:35 +0530

    completed summary container with total, subtotal

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -2,14 +2,27 @@ import { connect } from 'react-redux';
 import { Summary } from '../components/Summary';
 
 const mapStateToProps = (state) => {
-  let subtotal = 0;
   const items = state.items;
 
+  let subtotal = 0;
   for (const item of items) {
     subtotal += item.quantity * item.price;
   }
 
-  console.log(subtotal);
+  // const subtotal = items.reduce(
+  //   (sum, item) => sum + item.price * item.quantity,
+  //   0
+  // );
+
+  const tipAmount = subtotal * (state.tipPercentage / 100);
+
+  const total = subtotal + tipAmount;
+
+  return {
+    subtotal,
+    tipAmount,
+    total
+  };
 };
 
 export const SummaryContainer = connect(mapStateToProps)(Summary);
